9f444d91acbcf8735da3a405a7e56afb735512d9
Vinh_Trong_Nguyen <vinhtrong782002@gmail.com>
2024-03-02 16:31:48 +0700

    upload source code

---
+++ main.py
@@ -0,0 +1,83 @@
+import sys
+from Adafruit_IO import MQTTClient
+import time
+import random
+from uart import *
+
+AIO_FEED_IDs = ["nutnhan1", "sensor_iot1", "sensor_iot2", "sensor_iot3"]
+AIO_USERNAME = "vinhtrong782002"
+AIO_KEY = "aio_BsLO41VhInxiICw2ugBNjhPPfEc4"
+
+def connected(client):
+    print("Ket noi thanh cong ...")
+    for topic in AIO_FEED_IDs:
+        client.subscribe(topic)
+
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload + " , feed id: " + feed_id)
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+counter = 10
+sensor_type = 0
+def connected(client):
+    print("Ket noi thanh cong ...")
+    for topic in AIO_FEED_IDs:
+        client.subscribe(topic)
+
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload + " , feed id: " + feed_id)
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+counter = 10
+sensor_type = 0
+
+while True:
+    # counter = counter -1
+    # if counter <= 0:
+    #     counter = 10
+    #     print("Random data is publishing.....")
+    #     if sensor_type == 0:
+    #         print("Temperature.....")
+    #         temp = random.randint(10, 20)
+    #         client.publish("Temperature", temp)
+    #         sensor_type = 1
+    #     elif sensor_type == 1:
+    #         print("Humi.....")
+    #         humi = random.randint(50, 70)
+    #         client.publish("Humidity", humi)
+    #         sensor_type = 2
+    #     elif sensor_type == 2:
+    #         print("Light.....")
+    #         light = random.randint(100, 500)
+    #         client.publish("Light", light) 
+    #         sensor_type = 0
+    
+    readSerial(client)
+    time.sleep(1)
+    # pass
